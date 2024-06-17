80d12fd82f3ae467595baeb05d567db3bef75c2a
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-16 20:20:35 -0700

    attewmpt fix of s3

--- config/plugins.js
+++ config/plugins.js
@@ -4,8 +4,6 @@ module.exports = ({ env }) => ({
         provider: 'aws-s3',
         providerOptions: {
           s3Options: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET'),
             region: env('AWS_REGION'),
             params: {
               Bucket: env('AWS_BUCKET_NAME'),
