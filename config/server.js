84e98e00af8cf96c491a47a68ab9e7e00395c10f
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-20 16:19:34 -0700

    ssl and proxy for prod

--- config/server.js
+++ config/server.js
@@ -8,4 +8,8 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  proxy: {
+    enabled: true,
+    ssl: true,
+  },
 });
