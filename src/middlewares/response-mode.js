c5e1cb4479d94f85ffe0c0603c07c8c6830450ad
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-20 20:17:55 -0700

    more trying

--- src/middlewares/response-mode.js
+++ src/middlewares/response-mode.js
@@ -8,7 +8,16 @@ module.exports = (config, { strapi }) => {
     // Add your own logic here.
     return async (ctx, next) => {
         // Add Request-Specific Logic here
-      strapi.log.info('In a custom Global middleware.');
+      strapi.log.info('In a custom Global middleware.', ctx.request.url);
+      if (ctx.request.url.includes('/connect/apple')) {
+        strapi.log.info('Connecting to Apple Inc', ctx.request.url);
+        if (!ctx.request.url.includes('response_mode=form_post')) {
+            const separator = ctx.request.url.includes('?') ? '&' : '?';
+            ctx.request.url += `${separator}response_mode=form_post`;
+
+        strapi.log.info('Updated url', ctx.request.url);
+          }
+      }
                   ctx.request 
   
       await next();
