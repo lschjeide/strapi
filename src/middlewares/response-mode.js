2ade904fa609e14ead545f9b7fcfc65cab5dfa62
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-20 20:54:15 -0700

    progress

--- src/middlewares/response-mode.js
+++ src/middlewares/response-mode.js
@@ -11,12 +11,19 @@ module.exports = (config, { strapi }) => {
       strapi.log.info(`In a custom Global middleware, ${ctx.request.url}`);
       if (ctx.request.url.includes('/api/connect/apple/callback')) {
         strapi.log.info('Connecting to Apple Inc', ctx.request.url);
-        if (!ctx.request.url.includes('response_mode=form_post')) {
-            const separator = ctx.request.url.includes('?') ? '&' : '?';
-            ctx.request.url += `${separator}response_mode=form_post`;
-
-        strapi.log.info(`Updated url, ${ctx.request.url}`);
-          }
+      
+      // Check if the response_mode=form_post is already present
+      if (!ctx.request.query.response_mode || ctx.request.query.response_mode !== 'form_post') {
+        const parsedUrl = new URL(ctx.request.url, `https://${ctx.request.headers.host}`);
+        
+        // Append response_mode=form_post if not present
+        parsedUrl.searchParams.set('response_mode', 'form_post');
+        
+        // Update ctx.request.url with the new URL
+        ctx.request.url = parsedUrl.pathname + parsedUrl.search;
+        
+        strapi.log.info(`Updated URL, ${ctx.request.url}`);
+      }
       }
     ctx.request 
   
