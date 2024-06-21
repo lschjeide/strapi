27d4e9d861898d29e00039e15883e8a81b84ade4
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-20 21:05:31 -0700

    progress

--- src/middlewares/response-mode.js
+++ src/middlewares/response-mode.js
@@ -1,14 +1,14 @@
 'use strict';
-const qs = require('qs');
+
 /**
  * `assign-owner` middleware.
  */
 
-module.exports = (config, { strapi }) => {
+/*module.exports = (config, { strapi }) => {
     // Add your own logic here.
     return async (ctx, next) => {
         // Add Request-Specific Logic here
-    /*  strapi.log.info(`In a custom Global middleware, ${ctx.request.url}`);
+     strapi.log.info(`In a custom Global middleware, ${ctx.request.url}`);
       if (ctx.request.url.includes('/api/connect/apple/callback')) {
         strapi.log.info('Connecting to Apple Inc', ctx.request.url);
       
@@ -25,11 +25,22 @@ module.exports = (config, { strapi }) => {
         strapi.log.info(`Updated URL, ${ctx.request.url}`);
       }
       }
-    ctx.request */
+    ctx.request 
   
       await next();
   
                   // Add Response-Specific Logic here
+    };
+  };*/
+
+  module.exports = (config, { strapi }) => {
+    return async (ctx, next) => {
+      strapi.log.info(`Middleware triggered for URL: ${ctx.request.url}`);
+      
+      // Proceed to the next middleware or route
+      await next();
+      
+      strapi.log.info(`After middleware processing for URL: ${ctx.request.url}`);
     };
   };
   
