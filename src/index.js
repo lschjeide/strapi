ac74e6bba873bbfe3afa88b0bedf60662c383ea6
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-21 11:47:24 -0700

    fix client secret stuff

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ module.exports = {
             enabled: true,
             icon: 'apple',
             key: process.env.APPLE_CLIENT_ID,
-            secret: appleClientSecret,
+            clientSecret: appleClientSecret,
             callback: `${process.env.BASE_URL}/api/connect/apple/callback`,
             authorize_url: `https://appleid.apple.com/auth/authorize`,
             access_url: 'https://appleid.apple.com/auth/token',
