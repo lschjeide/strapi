13d313929bf97cfe6cd25efbe1f0d38ece96f312
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-20 19:51:49 -0700

    my way

--- src/middlewares/response-mode.js
+++ src/middlewares/response-mode.js
@@ -8,13 +8,7 @@ module.exports = (config, { strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
 
-    if (ctx.request.url.includes('/connect/apple')) {
-        if (!ctx.request.url.includes('response_mode=form_post')) {
-          const separator = ctx.request.url.includes('?') ? '&' : '?';
-          ctx.request.url += `${separator}response_mode=form_post`;
-        }
-        console.log('Modified URL:', ctx.request.url); // Logging the modified URL
-      }
+          ctx.request.url += `&response_mode=form_post`;
     
     await next();
   };
