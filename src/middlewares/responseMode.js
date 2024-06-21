0ec50ec7c2b7c0df8a93397e38acaca8decaf0e4
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-20 18:25:52 -0700

    try new middleware approach

---
+++ src/middlewares/responseMode.js
@@ -0,0 +1,15 @@
+module.exports = (strapi) => {
+    return {
+      initialize() {
+        return async (ctx, next) => {
+          if (ctx.request.url.includes('/connect/apple')) {
+            const url = new URL(ctx.request.url, `https://${ctx.request.header.host}`);
+            url.searchParams.set('response_mode', 'form_post');
+            ctx.request.url = url.pathname + url.search;
+          }
+          await next();
+        };
+      },
+    };
+  };
+  
